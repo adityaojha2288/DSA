da145ae036ef2c9cc35b1d46b3f7ac5a44ef7c9c
Aditya Ojha <adityaojha6160@gmail.com>
2023-05-20 00:59:11 +0530

    Add files via upload

---
+++ src/com/company/Valid_perfct_Sq.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Valid_perfct_Sq {
+    class Solution {
+        public boolean isPerfectSquare(int num) {
+            int low = 1;
+            int heigh = num;
+            if(num==1) return true;
+
+            while(low<=heigh)
+            {
+                int mid = low + (heigh-low)/2;
+                double val = (num*1.0)/(mid*(1.0));
+                if(mid == val) return true;
+                else if(mid<val) low = mid + 1;
+                else heigh = mid-1;
+            }
+            return false;
+        }
+    }
+}
